2222801b4e26c6e0dc0947b01f9416417c537d01
Alex <klinachevav@mail.ru>
2022-03-11 10:27:19 +0300

    fix second task

--- byePrinter/src/main/java/ru/itmo/osgi/bye/printer/ByePrinter.java
+++ byePrinter/src/main/java/ru/itmo/osgi/bye/printer/ByePrinter.java
@@ -1,9 +1,5 @@
 package ru.itmo.osgi.bye.printer;
 
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-
 public interface ByePrinter {
-    @Reference
     void print();
 }
--- byePrinterUser/src/main/java/ru/itmo/osgi/bye/user/ByeApplier.java
+++ byePrinterUser/src/main/java/ru/itmo/osgi/bye/user/ByeApplier.java
@@ -1,23 +1,27 @@
 package ru.itmo.osgi.bye.user;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 import ru.itmo.osgi.bye.printer.ByePrinter;
 
 @Component
 public class ByeApplier {
+    private ByePrinter printer;
+
+    @Reference(service = ByePrinter.class)
+    public void setByePrinter(ByePrinter printer) {
+        this.printer = printer;
+    }
+
+    public void unbindByePrinter(ByePrinter printer) {
+        this.printer = null;
+    }
+
     @Deactivate
-    public void deactivate(ComponentContext context) {
-        BundleContext bundleContext = context.getBundleContext();
-        ServiceReference<?> serviceReference =
-                bundleContext.getServiceReference(ByePrinter.class.getName());
-        ByePrinter printer = (ByePrinter) bundleContext.getService(serviceReference);
-        if (printer == null) {
-            return;
+    public void deactivate() {
+        if (printer != null) {
+            printer.print();
         }
-        printer.print();
     }
 }
